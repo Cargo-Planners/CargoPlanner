916cfacb97c6370c35576b0f0e43c9b7e3807868
shohamD1121 <shohamdouek@gmail.com>
2022-09-06 18:57:50 +0300

    blob added for test just incase we'll something like it later

--- src/components/ChoosePlatform/ChoosePlatformComponent.js
+++ src/components/ChoosePlatform/ChoosePlatformComponent.js
@@ -5,7 +5,7 @@ import karnafImg from "../../images/karnafIcon.png";
 const ChoosePlatformComponent = () => {
   return (
     <div className="h-screen w-screen coolGrad flex justify-center items-center">
-      <div className="flex flex-col gap-10 ">
+      <div className="flex flex-col gap-10 z-10">
         <h1 className="text-white text-5xl font-bold text-center">בחירת פלטפורמה</h1>
         <div className="flex flex-col gap-6">
           <OptionWrapper name="קרנף" img={karnafImg} />
