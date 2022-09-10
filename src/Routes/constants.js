cfb27bcf9dfcc557a9eb46e48f9d55956f082d72
shohamD1121 <shohamdouek@gmail.com>
2022-09-10 20:45:44 +0300

    added icons

--- src/Routes/constants.js
+++ src/Routes/constants.js
@@ -1,7 +1,7 @@
 const mainRoute = "/";
 const homeRoute = "/home";
 const newItemRoute = "/home/newItem";
-const BasicDataRoute = "/basicData"
+const BasicDataRoute = "/basicData";
 
 
 export const routeConstants = { mainRoute, homeRoute, newItemRoute, BasicDataRoute };
