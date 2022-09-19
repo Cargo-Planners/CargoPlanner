7222269a8cb80d77c70421eeb86872313fac9440
Or Kattan <ork123411@gmail.com>
2022-09-19 23:30:24 +0300

    working, basic data button chance, english and styling fixes

--- src/components/Grid/EditBasicDataButton.js
+++ src/components/Grid/EditBasicDataButton.js
@@ -11,11 +11,12 @@ const EditBasicDataButton = ({ img }) => {
       <button onClick={() => setModalIsOpen(true)}>
         <img className="h-20 w-20 mx-auto" src={img} alt="" />
       </button>
-<<<<<<< HEAD:src/components/Grid/EditBasicDataButton.js
-      <Modal isOpen={modalIsOpen} ariaHideApp={false} className="modal flex">
-=======
-      <Modal isOpen={modalIsOpen} className="modal flex" appElement={document.getElementById('root') || undefined}>
->>>>>>> 8aa4c19bb5d52e0f55b9135021f3f43de281e04f:src/components/Grid/EditStaticDataButton.js
+
+      <Modal
+        isOpen={modalIsOpen}
+        className="modal flex"
+        appElement={document.getElementById("root") || undefined}
+      >
         <BasicData setModalIsOpen={setModalIsOpen} />
       </Modal>
     </Fragment>
