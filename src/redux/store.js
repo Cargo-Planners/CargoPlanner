00d7afa926296f85d55602ba0e1e87c77647f996
shohamD1121 <shohamdouek@gmail.com>
2022-10-19 18:54:36 +0300

    deleted some files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import ObjectsDataSliceReducer from "./ObjectsDataSlice";
 import EditBasicDataReducer from "./EditBasicDataSlice";
-import fabricRefSliceReducer from "./fabricRefSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
     objectsData: ObjectsDataSliceReducer,
     basicData: EditBasicDataReducer,
-    fabricRef : fabricRefSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
